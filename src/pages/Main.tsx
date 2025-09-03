3702dcf0f2e3a0a30dc97deb33d3683ae32e2ef8
JYB0535 <jnk8528@gmail.com>
2025-09-03 16:22:40 +0900

    2025-09-03

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,14 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import { useState } from "react";
+import type { Post } from "../type";
+
 export default function Main() {
+    const [data, setData] = useState<Post[]>([]);
+
+    return(
+        <>
+        
+        </>
+    )
      
 }
