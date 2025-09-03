2bb07d784efdb053d2ddfecef8b5087966686197
JYB0535 <jnk8528@gmail.com>
2025-09-03 16:46:34 +0900

    2025-09-03

---
+++ src/api/postApi.ts
@@ -0,0 +1,27 @@
+const getPostDummy = [
+    {
+        id: 1,
+        name: 'abc',
+        date: '2023',
+        postName: 'abc의 글입니다'
+    },
+
+    {
+        id: 2,
+        name: 'bcd',
+        date: '2023',
+        postName: 'bcd의 글입니다'
+    },
+
+    {
+        id: 3,
+        name: 'cde',
+        date: '2023',
+        postName: 'cde의 글입니다'
+    },
+
+
+
+
+
+]
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,24 @@
-import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+
 import { useState } from "react";
 import type { Post } from "../type";
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridColDef } from "@mui/x-data-grid";
 
 export default function Main() {
     const [data, setData] = useState<Post[]>([]);
 
+    const columns: GridColDef[] = [
+        {field: 'name', headerName: '이름'}
+    ]
+        
+
     return(
         <>
+            <DataGrid 
+                rows={data}
+                columns={columns}
+                showToolbar
+            />
         
         </>
     )
