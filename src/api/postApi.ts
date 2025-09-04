fd04b4f44a55dfbe503288c282d243907f365f66
JYB0535 <jnk8528@gmail.com>
2025-09-04 19:48:54 +0900

    2025-09-0

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,32 +1,41 @@
+import axios from "axios";
 import type { Post } from "../type";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getData = async (): Promise<Post[]> => {
 
-const getPostDummy = [
-    {
-        id: 1,
-        name: 'abc',
-        date: 2023,
-        postName: 'abc의 글입니다',
-        view: 23
-    },
 
-    {
-        id: 2,
-        name: 'bcd',
-        date: 2024,
-        postName: 'bcd의 글입니다',
-        view: 7000
-    },
+  const response = await axios.post(`${BASE_URL}/`);
+  return response.data;
 
-    {
-        id: 3,
-        name: 'cde',
-        date: 2025,
-        postName: 'cde의 글입니다',
-        view: 100000
-    },
+}
 
-]
-return Promise.resolve(getPostDummy);
-} 
+
+
+// const getPostDummy = [
+//     {
+//         id: 1,
+//         name: 'abc',
+//         date: 2023,
+//         postName: 'abc의 글입니다',
+//         view: 23
+//     },
+
+//     {
+//         id: 2,
+//         name: 'bcd',
+//         date: 2024,
+//         postName: 'bcd의 글입니다',
+//         view: 7000
+//     },
+
+//     {
+//         id: 3,
+//         name: 'cde',
+//         date: 2025,
+//         postName: 'cde의 글입니다',
+//         view: 100000
+//     },
+
+// ]
+//return Promise.resolve(getPostDummy);
